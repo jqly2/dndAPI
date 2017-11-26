0848631e910d1ac495b7500180c241866db6acfc
jqly2 <jonathanly213@gmail.com>
2017-11-26 11:32:28 -0800

    animations added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,40 @@
 import { Component } from '@angular/core';
-import {DnDataService} from './dn-data.service';
+import { DnDataService } from './dn-data.service';
 import { HttpClient } from '@angular/common/http';
-
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations'
+import { animation } from './animation'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   providers:[DnDataService],
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[animation],
 })
 export class AppComponent{
 
-  spells: string;
+  spells: string[];
+
+  // state: string ='small';
 
   constructor(private http:HttpClient, private dataService: DnDataService) {}
-
-
-  
 
   getSpells() {
     this.dataService.getSpellData().subscribe((data) => {
       console.log(data);
       this.spells = data;
+      this.spells = this.spells.map((spell)=> {
+        spell.state = 'small';
+        return spell;
+      })
     })
+
   }
+
+  zoomCard(index){
+    index.state = (index.state === 'small' ? 'large' : 'small')
+  }
+
 
   title = 'the Library';
 }
